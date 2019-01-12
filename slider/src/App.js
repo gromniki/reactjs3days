e5e3ceb7e7b5348896f3b34afe871624f0ce9f06
Roman Golubev <gromniki@gmail.com>
2019-01-12 16:33:42 +0300

    added react-bootstrap

--- slider/src/App.js
+++ slider/src/App.js
@@ -2,27 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main className="wrapper">
+          <SayFullName name='Роман' surname='Голубев' link='vk.com/gromniki' />
+          <SayFullName name='Даниил' surname='Иванов' link='facebook.com' />
+          <SayFullName name='Василиса' surname='Прекрасная' link='twitter.com' />
+      </main>
     );
   }
+}
+
+
+
+function SayFullName(props) {  // props в данном случае – это объект не динамический, только чтение
+  return (
+      <section>
+          <h1>Меня зовут {props.name}, а фамилия – {props.surname}</h1>
+          <a href={props.link}>Ссылка на мой профиль</a>
+      </section>  // у реакта есть особенность, он вставляет именно блок, поэтому нужно обернуть все теги в <div></div>
+  )
 }
 
 export default App;
--- slider/src/index.js
+++ slider/src/index.js
@@ -4,35 +4,33 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function Hello() {
-    let phrase = 'World!!!'
-    return (
-        <h1>Hello {phrase}</h1>
-    )
-}
+// function Hello() {
+//     let phrase = 'World!!!'
+//     return (
+//         <h1>Hello {phrase}</h1>
+//     )
+// }
 
-function SayFullName(props) {  // props в данном случае – это объект не динамический, только чтение
-    return (
-        <section>
-            <h1>Меня зовут {props.name}, а фамилия – {props.surname}</h1>
-            <a href={props.link}>Ссылка на мой профиль</a>
-        </section>  // у реакта есть особенность, он вставляет именно блок, поэтому нужно обернуть все теги в <div></div>
-    )
-}
+// function SayFullName(props) {  // props в данном случае – это объект не динамический, только чтение
+//     return (
+//         <section>
+//             <h1>Меня зовут {props.name}, а фамилия – {props.surname}</h1>
+//             <a href={props.link}>Ссылка на мой профиль</a>
+//         </section>  // у реакта есть особенность, он вставляет именно блок, поэтому нужно обернуть все теги в <div></div>
+//     )
+// }
 
-function MeetAll() {
-    return (
-        <main>
-            <SayFullName name='Роман' surname='Голубев' link='vk.com/gromniki' />
-            <SayFullName name='Даниил' surname='Иванов' link='facebook.com' />
-            <SayFullName name='Василиса' surname='Прекрасная' link='twitter.com' />
-        </main>
-    )
-}
+// function MeetAll() {
+//     return (
+//         <main>
+//             <SayFullName name='Роман' surname='Голубев' link='vk.com/gromniki' />
+//             <SayFullName name='Даниил' surname='Иванов' link='facebook.com' />
+//             <SayFullName name='Василиса' surname='Прекрасная' link='twitter.com' />
+//         </main>
+//     )
+// }
 
-const element = <MeetAll />
-
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // Всё, что написано выше – функциональный стиль
 // В Реакте, в основном, принято писать в объектном стиле
