aa8d93ebbdf4c9b75182a5394076ef25519e8830
Roman Golubev <gromniki@gmail.com>
2019-01-12 19:33:36 +0300

    added components

--- slider/src/App.js
+++ slider/src/App.js
@@ -3,30 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
+
+import styled from 'styled-components';
+
+import Header from './components/Header';
+import Main from './components/Main';
+import Menu from './components/Menu';
 
 class App extends Component {
-  render() {
-    return (
-      <main className="wrapper">
-          <SayFullName name='Роман' surname='Голубев' link='vk.com/gromniki' />
-          <SayFullName name='Даниил' surname='Иванов' link='facebook.com' />
-          <SayFullName name='Василиса' surname='Прекрасная' link='twitter.com' />
-      </main>
-    );
-  }
-}
-
-
-
-function SayFullName(props) {  // props в данном случае – это объект не динамический, только чтение
-  return (
-      <section>
-          <h1>Меня зовут {props.name}, а фамилия – {props.surname}</h1>
-          <a href={props.link}>Ссылка на мой профиль</a>
-      </section>  // у реакта есть особенность, он вставляет именно блок, поэтому нужно обернуть все теги в <div></div>
-  )
+    render() {
+        return (
+        <main className="App">          
+            <Header />
+            <Menu />
+            <Main />
+        </main>
+        );
+    }
 }
 
 export default App;
